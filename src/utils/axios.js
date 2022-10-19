2be652d2aeee0932d12cb5d9b1fd0a1b4e5d06e6
Dawood Bin Zafar <buttdawood18@gmail.com>
2022-10-19 12:36:45 +0500

    updated build

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,9 @@ import axioss from 'axios';
 
 
 // export const API_URL = 'http://192.168.1.191:3000/api/v1';
-export const API_URL = 'http://galileoargon.tk/api/v1/';
+// export const API_URL = 'http://galileoargon.tk/api/v1/';
+export const API_URL = 'https://app.galileoprotocol.io/api/v1/';
+
 // export const API_URL = 'http://192.168.1.191:3000/api/v1/';
 
 
