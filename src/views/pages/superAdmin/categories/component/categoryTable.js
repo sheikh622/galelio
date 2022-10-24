f1fbacc9fdf075993e684bfad972d16578b1533d
AimanZaheer04 <99002734+AimanZaheer04@users.noreply.github.com>
2022-10-24 17:59:38 +0500

    categoires state

--- src/views/pages/superAdmin/categories/component/categoryTable.js
+++ src/views/pages/superAdmin/categories/component/categoryTable.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useTheme } from '@mui/material/styles';
-import { IconButton, Stack, Table, TableBody, TableCell, TableContainer,Grid, Divider, Typography, TableHead, TableRow, Tooltip } from '@mui/material';
+import { IconButton, Stack, Table, TableBody, TableCell, TableContainer, Grid, Divider, Typography, TableHead, TableRow, Tooltip } from '@mui/material';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import DeleteCategoryDialog from './deleteCategoryDialog';
@@ -23,121 +23,121 @@ const CategoryTable = ({
     const [deleteId, setDeleteId] = useState(0);
     const [name, setName] = useState();
     const [profitPercentage, setProfitPercentage] = useState();
-// console.log(mainBrandId, "brand id================>")
+    // console.log(mainBrandId, "brand id================>")
 
     return (
-        <><AddUpdateCategory 
-        profitPercentage={profitPercentage}
-        mainBrandId={mainBrandId}
-        open={open}
-        deleteId={deleteId}
-        name={name}
-        setOpen={setOpen}
-        update={update}
-        setUpdate={setUpdate}
-        categoryList={categoryList}
-        page={page}
-        limit={limit}
-        search={search}
+        <><AddUpdateCategory
+            profitPercentage={profitPercentage}
+            mainBrandId={mainBrandId}
+            open={open}
+            deleteId={deleteId}
+            name={name}
+            setOpen={setOpen}
+            update={update}
+            setUpdate={setUpdate}
+            categoryList={categoryList}
+            page={page}
+            limit={limit}
+            search={search}
         /><TableContainer>
-            {/* delete */}
-            <DeleteCategoryDialog
-                mainBrandId={mainBrandId}
-                deleteId={deleteId}
+                {/* delete */}
+                <DeleteCategoryDialog
+                    mainBrandId={mainBrandId}
+                    deleteId={deleteId}
 
-                deleteOpen={deleteOpen}
-                setDeleteOpen={setDeleteOpen}
+                    deleteOpen={deleteOpen}
+                    setDeleteOpen={setDeleteOpen}
 
-                page={page}
-                limit={limit}
-                search={search} />
-            <Table>
-                <TableHead>
-                    <TableRow>
-                       
-                        <TableCell align="center">Name</TableCell>
-                        <TableCell align="center">Profit</TableCell>
+                    page={page}
+                    limit={limit}
+                    search={search} />
+                <Table>
+                    <TableHead>
+                        <TableRow>
 
-                        <TableCell align="center">Actions</TableCell>
-                    </TableRow>
-                </TableHead>
-                { categoryList.categories?.length > 0 ? (
-                <TableBody>
-                    {categoryList.categories != undefined &&
+                            <TableCell align="center">Name</TableCell>
+                            <TableCell align="center">Profit</TableCell>
 
-
-                        categoryList.categories.map((row, index) => (
-                            <>
-                                <TableRow onClick={() => {
-                                   
-                                }}>
-                                    
-                                    <TableCell align="center" sx={{ padding: '0px' }}>
-                                        {row.name}
-                                    </TableCell>
-                                    <TableCell align="center" sx={{ padding: '0px' }}>
-                                        {row.profitPercentage}%
-                                    </TableCell>
-
-                                    <TableCell align="center" sx={{ padding: '0px' }}>
-                                        <Stack direction="row" justifyContent="center" alignItems="center">
-                                            <Tooltip placement="top" title="Edit">
-                                                <IconButton
-                                                    color="primary"
-                                                    aria-label="Edit"
-                                                    size="large"
-                                                    onClick={() => {
-                                                        setName(row.name);
-                                                        setProfitPercentage(row.profitPercentage);
-                                                        setOpen(true);
-                                                        setUpdate(true);
-                                                        setDeleteId(row.id);
-                                                    }}
-                                                >
-                                                    <EditOutlinedIcon sx={{ fontSize: '1.5rem' }} />
-                                                </IconButton>
-                                            </Tooltip>
-                                            <Tooltip placement="top" title="Delete">
-                                                <IconButton
-                                                    color="primary"
-                                                    sx={{
-                                                        color: theme.palette.orange.dark,
-                                                        borderColor: theme.palette.orange.main,
-                                                        '&:hover ': { background: theme.palette.orange.light }
-                                                    }}
-                                                    size="large"
-                                                    onClick={() => {
-                                                        setDeleteId(row.id); //categoryid
-                                                        setDeleteOpen(true); //open delete dialoge
+                            <TableCell align="center">Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    {categoryList.categories?.length > 0 ? (
+                        <TableBody>
+                            {categoryList.categories != undefined &&
 
 
-                                                    }}
-                                                >
-                                                    <DeleteOutlineOutlinedIcon sx={{ fontSize: '1.5rem' }} />
-                                                </IconButton>
-                                            </Tooltip>
-                                        </Stack>
-                                    </TableCell>
-                                </TableRow>
-                            </>
-                        ))}
-                </TableBody>
-                 ) : (
-                    <>
-                        <Grid item md={12}>
-                            <Divider />
-                        </Grid>
-                        <Grid item>
-                            <Typography style={{ padding: '20px', fontWeight: '800' }}> No Data Available</Typography>
-                        </Grid>
-                        
-                       
-                    
-                    </>
-                )} 
-            </Table>
-            <Divider />
-        </TableContainer></>
+                                categoryList.categories.map((row, index) => (
+                                    <>
+                                        <TableRow onClick={() => {
+
+                                        }}>
+
+                                            <TableCell align="center" sx={{ padding: '0px' }}>
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell align="center" sx={{ padding: '0px' }}>
+                                                {row.profitPercentage}%
+                                            </TableCell>
+
+                                            <TableCell align="center" sx={{ padding: '0px' }}>
+                                                <Stack direction="row" justifyContent="center" alignItems="center">
+                                                    <Tooltip placement="top" title="Edit">
+                                                        <IconButton
+                                                            color="primary"
+                                                            aria-label="Edit"
+                                                            size="large"
+                                                            onClick={() => {
+                                                                setName(row.name);
+                                                                setProfitPercentage(row.profitPercentage);
+                                                                setOpen(true);
+                                                                setUpdate(true);
+                                                                setDeleteId(row.id);
+                                                            }}
+                                                        >
+                                                            <EditOutlinedIcon sx={{ fontSize: '1.5rem' }} />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                    <Tooltip placement="top" title="Delete">
+                                                        <IconButton
+                                                            color="primary"
+                                                            sx={{
+                                                                color: theme.palette.orange.dark,
+                                                                borderColor: theme.palette.orange.main,
+                                                                '&:hover ': { background: theme.palette.orange.light }
+                                                            }}
+                                                            size="large"
+                                                            onClick={() => {
+                                                                setDeleteId(row.id); //categoryid
+                                                                setDeleteOpen(true); //open delete dialoge
+
+
+                                                            }}
+                                                        >
+                                                            <DeleteOutlineOutlinedIcon sx={{ fontSize: '1.5rem' }} />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </Stack>
+                                            </TableCell>
+                                        </TableRow>
+                                    </>
+                                ))}
+                        </TableBody>
+                    ) : (
+                        <>
+                            <Grid item md={12}>
+                                <Divider />
+                            </Grid>
+                            <Grid item>
+                                <Typography style={{ padding: '20px', fontWeight: '800' }}> No Data Available</Typography>
+                            </Grid>
+
+
+
+                        </>
+                    )}
+                </Table>
+                <Divider />
+            </TableContainer></>
     );
 };
 
