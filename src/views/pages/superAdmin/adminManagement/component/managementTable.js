d3dce48e9212927153d197cfcea8adb63d85a231
AimanZaheer04 <99002734+AimanZaheer04@users.noreply.github.com>
2022-10-31 11:34:33 +0500

    update bug status

--- src/views/pages/superAdmin/adminManagement/component/managementTable.js
+++ src/views/pages/superAdmin/adminManagement/component/managementTable.js
@@ -1,7 +1,23 @@
 import { useState } from 'react';
 import { useTheme } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
-import {Divider,Typography, IconButton, Stack,Grid, Table, TableBody,Menu, TableCell,MenuItem, TableContainer, TableHead,Button, TableRow, Tooltip } from '@mui/material';
+import {
+    Divider,
+    Typography,
+    IconButton,
+    Stack,
+    Grid,
+    Table,
+    TableBody,
+    Menu,
+    TableCell,
+    MenuItem,
+    TableContainer,
+    TableHead,
+    Button,
+    TableRow,
+    Tooltip
+} from '@mui/material';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import DeleteManagementDialog from './deleteManagementDialog';
@@ -12,12 +28,13 @@ import AddUpdateDialog from './addUpdateManagement';
 import MiniteringDialog from './minitingRole';
 import BlockUnblockDialog from './blockUnblock';
 import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 import { useNavigate } from 'react-router-dom';
 const AdminTable = ({ page, limit, search, open, setOpen, brandId, setBrandId }) => {
     const theme = useTheme();
     const navigate = useNavigate();
-   
+
     const [deleteOpen, setDeleteOpen] = useState(false);
     const [minitering, setMinitering] = useState(false);
     const [isBlock, setIsBlock] = useState('');
@@ -27,7 +44,7 @@ const AdminTable = ({ page, limit, search, open, setOpen, brandId, setBrandId })
     const [lname, setLname] = useState();
     const [email, setEmail] = useState();
     const [id, setId] = useState();
-   
+    const [detailId, setDetailId] = useState();
     const [approve, setApprove] = useState(false);
     const [update, setUpdate] = useState(false);
     const [anchorEl, setAnchorEl] = useState(null);
@@ -37,27 +54,32 @@ const AdminTable = ({ page, limit, search, open, setOpen, brandId, setBrandId })
 
     const handleClose = () => {
         setAnchorEl(null);
-       
     };
-   
-   
+    const openDetails = (id) => {
+        console.log(id, 'open=');
+        if (detailId === id) {
+            setDetailId(null);
+        } else {
+            setDetailId(id);
+        }
+    };
+
     const adminList = useSelector((state) => state.adminReducer.adminsList);
-    console.log(adminList,"===============adminList===========================>");
+    console.log(adminList, '===============adminList===========================>');
     return (
         <TableContainer>
-        <AddUpdateDialog
-        fname={fname}
-        lname={lname}
-        email={email}
-        page={page}
-        limit={limit}
-        setOpen={setOpen}
-        open={open}
-        update={update}
-        setUpdate={setUpdate}
-        search={search}
-        
-        />
+            <AddUpdateDialog
+                fname={fname}
+                lname={lname}
+                email={email}
+                page={page}
+                limit={limit}
+                setOpen={setOpen}
+                open={open}
+                update={update}
+                setUpdate={setUpdate}
+                search={search}
+            />
 
             <DeleteManagementDialog
                 deleteOpen={deleteOpen}
@@ -68,22 +90,22 @@ const AdminTable = ({ page, limit, search, open, setOpen, brandId, setBrandId })
                 search={search}
             />
             <BlockUnblockDialog
-            isBlock={isBlock}
-            email={email}
-            open={status}
-            setOpen={setStatus}
+                isBlock={isBlock}
+                email={email}
+                open={status}
+                setOpen={setStatus}
                 brandId={brandId}
                 page={page}
                 limit={limit}
                 search={search}
             />
             <MiniteringDialog
-            isMenu={isMenu}
-            setIsMenu={setIsMenu}
-            isBlock={isBlock}
-            open={minitering}
-            email={email}
-            setOpen={setMinitering}
+                isMenu={isMenu}
+                setIsMenu={setIsMenu}
+                isBlock={isBlock}
+                open={minitering}
+                email={email}
+                setOpen={setMinitering}
                 brandId={brandId}
                 page={page}
                 limit={limit}
@@ -92,111 +114,114 @@ const AdminTable = ({ page, limit, search, open, setOpen, brandId, setBrandId })
             <Table>
                 <TableHead>
                     <TableRow>
-
                         <TableCell align="center">First Name</TableCell>
                         <TableCell align="center">Last Name</TableCell>
                         <TableCell align="center">Email</TableCell>
                         <TableCell align="center">Status</TableCell>
                         <TableCell align="center">Minting Access</TableCell>
-                        
-                        
+
                         <TableCell align="center">Actions</TableCell>
                     </TableRow>
                 </TableHead>
-                {adminList.admins!= undefined && adminList.count > 0 ? (
-                <TableBody sx={{padding:"10px"}}>
+                {adminList.admins != undefined && adminList.count > 0 ? (
+                    <TableBody sx={{ padding: '10px' }}>
+                        {adminList.admins != undefined &&
+                            adminList.admins.map((row, index) => (
+                                <>
+                                    <TableRow>
+                                        <TableCell align="center">{row.firstName}</TableCell>
+                                        <TableCell align="center">{row.lastName}</TableCell>
+                                        <TableCell align="center">{row.email}</TableCell>
 
-                    {adminList.admins!= undefined &&
-                        adminList.admins.map((row, index) => (
-                            <TableRow>
+                                        <TableCell align="center">
+                                            {row.isActive == false && <Chip label="Blocked" size="small" chipcolor="success" />}
+                                            {row.isActive == true && <Chip label="Unblocked" size="small" chipcolor="orange" />}
+                                        </TableCell>
 
-                                <TableCell align="center" >
-                                    {row.firstName}
-                                </TableCell>
-                                <TableCell align="center" >
-                                    {row.lastName}
-                                </TableCell>
-                                <TableCell align="center" >
-                                   {row.email}
-                                </TableCell>
-                               
-                                <TableCell  align="center" >
-                               {row.isActive == false   && <Chip label="Blocked" size="small" chipcolor="success" />}
-                                {row.isActive == true  && <Chip label="Unblocked" size="small" chipcolor="orange" />} 
-                           
-                               
-                                
-                            </TableCell>
-                                    
-                             
-                                <TableCell  align="center" >
-                                 {row.hasMintingAccess == true   && <Chip label="Allowed" size="small" chipcolor="success" />}
-                                {row.hasMintingAccess == false  && <Chip label="Not Allowed" size="small" chipcolor="orange" />}
-                           
-                           
-                               
-                                    
-                                </TableCell>
-
-                               
-                                <TableCell align="center" >
-                                <Grid item>
-                                <MoreHorizOutlinedIcon
-                                    fontSize="small"
-                                    sx={{ color: theme.palette.grey[500], cursor: 'pointer' }}
-                                    aria-controls="menu-user-details-card"
-                                    aria-haspopup="true"
-                                    onClick={handleClick}
-                                />
-                                <Menu
-                                    id="menu-user-details-card"
-                                    anchorEl={anchorEl}
-                                    keepMounted
-                                    open={Boolean(anchorEl)}
-                                    onClose={handleClose}
-                                    variant="selectedMenu"
-                                    anchorOrigin={{
-                                        vertical: 'bottom',
-                                        horizontal: 'right'
-                                    }}
-                                    transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'right'
-                                    }}
-                                >
-                                    <MenuItem onClick={() => {
-                                        setOpen(true);
-                                        setUpdate(true);
-                                        setFname(row.firstName);
-                                        setLname(row.lastName);
-                                        setEmail(row.email);
-                                        setAnchorEl(null);
-                                    }}> edit</MenuItem>
-                                    <MenuItem onClick={() => {
-                                        setDeleteOpen(true);
-                                        setId(row.id);
-                                        setAnchorEl(null);
-                                    }}> Delete</MenuItem>
-                                    <MenuItem onClick={() => {
-                                        setStatus(true);
-                                        setEmail(row.email);
-                                        setAnchorEl(null);
-                                    }}> block Status </MenuItem>
-                                    <MenuItem onClick={() => {
-                                        setMinitering(true);
-                                        setEmail(row.email);
-                                        setAnchorEl(null);
-
-                                    }}> Minting Status </MenuItem>
-                                </Menu>
-                            </Grid>
-                                </TableCell>
-
-
-
-                            </TableRow>
-                        ))}
-                </TableBody>
+                                        <TableCell align="center">
+                                            {row.hasMintingAccess == true && <Chip label="Allowed" size="small" chipcolor="success" />}
+                                            {row.hasMintingAccess == false && <Chip label="Not Allowed" size="small" chipcolor="orange" />}
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            <Tooltip placement="top" title="View">
+                                                <IconButton
+                                                    color="primary"
+                                                    aria-label="detail"
+                                                    size="large"
+                                                    onClick={() => {
+                                                        openDetails(row.id);
+                                                    }}
+                                                >
+                                                    <KeyboardArrowDownIcon sx={{ fontSize: '1.5rem' }} />
+                                                </IconButton>
+                                            </Tooltip>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow style={{ display: detailId !== row.id ? 'none' : '' }}>
+                                        <TableCell sx={{ pl: 5 }} colSpan={12}>
+                                            <div>
+                                                <Grid container spacing={4}>
+                                                    <Grid item xs={3} md={3}>
+                                                        <Button
+                                                            variant="outlined"
+                                                            size="large"
+                                                            onClick={() => {
+                                                                setOpen(true);
+                                                                setUpdate(true);
+                                                                setFname(row.firstName);
+                                                                setLname(row.lastName);
+                                                                setEmail(row.email);
+                                                                setAnchorEl(null);
+                                                            }}
+                                                        >
+                                                            edit
+                                                        </Button>
+                                                    </Grid>
+                                                    <Grid item xs={3} md={3}>
+                                                        <Button
+                                                            variant="outlined"
+                                                            size="large"
+                                                            onClick={() => {
+                                                                setDeleteOpen(true);
+                                                                setId(row.id);
+                                                                setAnchorEl(null);
+                                                            }}
+                                                        >
+                                                            Delete
+                                                        </Button>
+                                                    </Grid>
+                                                    <Grid item xs={3} md={3}>
+                                                        <Button
+                                                            variant="outlined"
+                                                            size="large"
+                                                            onClick={() => {
+                                                                setStatus(true);
+                                                                setEmail(row.email);
+                                                            }}
+                                                        >
+                                                            block Status
+                                                        </Button>
+                                                    </Grid>
+                                                    <Grid item xs={3} md={3}>
+                                                        <Button
+                                                            variant="outlined"
+                                                            size="large"
+                                                            onClick={() => {
+                                                                setMinitering(true);
+                                                                setEmail(row.email);
+                                                            }}
+                                                        >
+                                                            {' '}
+                                                            Minting Status
+                                                        </Button>
+                                                    </Grid>
+                                                </Grid>
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                </>
+                            ))}
+                    </TableBody>
                 ) : (
                     <>
                         <Grid item md={12}>
