355447f642c6bdb4132bd2312c58dd9b7f3bf6ae
DESKTOP-5HGFKUS\Mehtab <mehtabgilani512@gmail.com>
2022-11-24 19:52:54 +0500

    changes

--- src/views/pages/brandAdmin/nftManagement/component/addNft.js
+++ src/views/pages/brandAdmin/nftManagement/component/addNft.js
@@ -42,44 +42,55 @@ import fileFill from '@iconify-icons/eva/file-fill';
 import closeFill from '@iconify-icons/eva/close-fill';
 import { fData } from 'utils/formatNumber';
 import QuantitySelector from './quantitySelector';
+import { useEffect } from 'react';
+
+const typeArray = [
+    {
+        value: 'ETH',
+        label: 'ETH'
+    },
+    {
+        value: 'USDT',
+        label: 'USDT'
+    }
+];
 
 export default function AddNft({ open, setOpen }) {
+    const dispatch = useDispatch();
     const [mintType, setMintType] = useState('directMint');
-    const [fields, setFields] = useState([]);
     const [uploadedImages, setUploadedImages] = useState([]);
     const [fieldDataArray, setFieldDataArray] = useState([]);
-    const [currencyType, setCurrencyType] = useState([]);
+    const [type, setType] = useState('ETH');
+    const handleType = (event) => {
+        setType(event.target.value);
+    };
 
-    const theme = useTheme();
-    const dispatch = useDispatch();
     const validationSchema = Yup.object({
-        name: Yup.string()
-            .required('Brand Name is required!')
-            .max(42, 'Brand Name can not exceed 42 characters')
-            .matches(/^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$/, 'Invalid Brand name')
+        nftName: Yup.string()
+            .required('NFT Name is required!')
+            .max(42, 'NFT Name can not exceed 42 characters')
+            .matches(/^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$/, 'Invalid NFT name'),
+        nftDescription: Yup.string()
+            .required('NFT Description is required!')
+            .max(500, 'Invalid NFT description can not exceed 500 characters')
+            .matches(/^[-a-zA-Z0-9-()]+(\s+[-a-zA-Z0-9-()]+)*$/, 'Invalid NFT description'),
+        nftPrice: Yup.number()
+            .min(0.000001, 'Price should not less than zero')
+            .required('NFT Price is required')
+            .typeError('Invalid Price'),
+        images: Yup.mixed()
     });
     const formik = useFormik({
         enableReinitialize: true,
         initialValues: {
-            name: '',
-            price: '',
-            currency_type: '',
-            description: '',
+            nftName: '',
+            nftDescription: '',
+            nftPrice: 0,
             images: []
         },
-        // validationSchema,
+        validationSchema,
         onSubmit: (values) => {
-            const data = {
-                mintType: mintType,
-                name: values.name,
-                price: values.price,
-                currencyType: currencyType,
-                description: values.description,
-                image: values.images[0],
-                fieldData: fieldDataArray
-            };
-
-            console.log('Formik onSubmit in addNft', data);
+            dispatch();
         }
     });
 
@@ -125,29 +136,36 @@ export default function AddNft({ open, setOpen }) {
         setFieldDataArray(array);
     };
 
-    console.log('main fieldDataArray', fieldDataArray);
-
-    const handleCurrencyTypeChange = (e) => {
-        let ct = e.target.value;
-        setCurrencyType(ct);
+    const handleRemoveField = (index) => {
+        console.log({ index });
+        let array = [...fieldDataArray];
+        array.splice(index, 1);
+        console.log({ array });
+        setFieldDataArray(array);
     };
+
+    useEffect(() => {
+        console.log('fieldDataArray', fieldDataArray);
+    }, [fieldDataArray]);
     return (
         <>
             <Dialog
-                style={{}}
                 open={open}
+                onClose={handleClose}
+                aria-labelledby="form-dialog-title"
+                className="brandDialog"
+                maxWidth="md"
                 TransitionComponent={Transition}
                 keepMounted
-                // onClose={handleClose}
-                aria-labelledby="alert-dialog-slide-title1"
                 aria-describedby="alert-dialog-slide-description1"
             >
-                <DialogTitle id="alert-dialog-slide-title1">{'Add NFT'}</DialogTitle>
+                <DialogTitle id="alert-dialog-slide-title1">Add NFT</DialogTitle>
 
-                <DialogContent style={{ width: '35rem' }}>
-                    <Grid container spacing={2} sx={{ ml: 1, mb: 2 }}>
-                        <Grid item xs={4}>
+                <DialogContent>
+                    <Grid container spacing={2} textAlign="end">
+                        <Grid item xs={12}>
                             <Button
+                                sx={{ marginRight: '10px' }}
                                 variant={mintType == 'directMint' ? 'contained' : 'outlined'}
                                 onClick={() => {
                                     setMintType('directMint');
@@ -155,8 +173,6 @@ export default function AddNft({ open, setOpen }) {
                             >
                                 Direct Mint
                             </Button>
-                        </Grid>
-                        <Grid item xs={4}>
                             <Button
                                 variant={mintType == 'lazyMint' ? 'contained' : 'outlined'}
                                 onClick={() => {
@@ -168,156 +184,138 @@ export default function AddNft({ open, setOpen }) {
                         </Grid>
                     </Grid>
                     <form autoComplete="off" onSubmit={formik.handleSubmit}>
-                        <Grid container>
-                            <Grid xs={5}>
+                        <Grid container mt={2}>
+                            <Grid xs={4} mt={2} pr={3}>
                                 <TextField
-                                    sx={{ marginTop: '25px' }}
-                                    id="name"
-                                    name="name"
+                                    id="nftName"
+                                    name="nftName"
                                     label="NFT Name"
-                                    value={formik.values.name}
+                                    fullWidth
+                                    value={formik.values.nftName}
                                     onChange={formik.handleChange}
-                                    // error={formik.touched.name && Boolean(formik.errors.name)}
-                                    // helperText={formik.touched.name && formik.errors.name}
-                                    // fullWidth
+                                    error={formik.touched.nftName && Boolean(formik.errors.nftName)}
+                                    helperText={formik.touched.nftName && formik.errors.nftName}
                                     autoComplete="given-name"
                                     variant="standard"
                                 />
                             </Grid>
 
-                            <Grid xs={3}>
+                            <Grid xs={4} mt={2} pr={3}>
                                 <TextField
-                                    sx={{ marginTop: '25px' }}
-                                    id="price"
-                                    name="price"
+                                    id="nftPrice"
+                                    name="nftPrice"
                                     label="NFT Price"
-                                    // value=" "
+                                    fullWidth
+                                    value={formik.values.nftPrice}
                                     onChange={formik.handleChange}
-                                    // error={formik.touched.name && Boolean(formik.errors.name)}
-                                    // helperText={formik.touched.name && formik.errors.name}
-                                    // fullWidth
+                                    error={formik.touched.nftPrice && Boolean(formik.errors.nftPrice)}
+                                    helperText={formik.touched.nftPrice && formik.errors.nftPrice}
+                                    autoComplete="given-name"
                                     variant="standard"
                                 />
                             </Grid>
 
-                            <Grid xs={4}>
-                                <FormControl fullWidth sx={{ marginTop: '25px' }}>
-                                    <InputLabel id="demo-simple-select-label">Currency Type</InputLabel>
-                                    <Select
-                                        labelId="demo-simple-select-label"
-                                        id="currencyType"
-                                        name=""
-                                        // label="currencyType"
-                                        // value= "Currency Type"
-                                        onChange={handleCurrencyTypeChange}
-                                    >
-                                        <MenuItem value={'ETH'}>ETH</MenuItem>
-                                        <MenuItem value={'USDT'}>USDT</MenuItem>
-                                    </Select>
-                                </FormControl>
+                            <Grid xs={4} mt={2} pr={3}>
+                                <TextField
+                                    id="outlined-select-budget"
+                                    select
+                                    fullWidth
+                                    label="Select Token"
+                                    value={type}
+                                    onChange={handleType}
+                                >
+                                    {typeArray.map((option, index) => (
+                                        <MenuItem key={index} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                            </Grid>
+                            <Grid xs={12} mt={2} pr={3}>
+                                <TextField
+                                    multiline
+                                    rows={2}
+                                    id="nftDescription"
+                                    name="nftDescription"
+                                    label="NFT Description"
+                                    fullWidth
+                                    value={formik.values.nftDescription}
+                                    onChange={formik.handleChange}
+                                    error={formik.touched.nftDescription && Boolean(formik.errors.nftDescription)}
+                                    helperText={formik.touched.nftDescription && formik.errors.nftDescription}
+                                    autoComplete="given-name"
+                                    variant="standard"
+                                />
+                            </Grid>
+                            <Grid xs={12} mt={2} pr={3}>
+                                <Button
+                                    variant="contained"
+                                    sx={{ float: 'right' }}
+                                    onClick={() => {
+                                        setFieldDataArray([
+                                            ...fieldDataArray,
+                                            {
+                                                fieldName: '',
+                                                fieldValue: ''
+                                            }
+                                        ]);
+                                    }}
+                                >
+                                    Add Fields
+                                </Button>
                             </Grid>
                         </Grid>
 
-                        {/* <TextField
-                            sx={{ marginTop: '25px' }}
-                            id="currency_type"
-                            name="currency_type"
-                            label="Currency Type"
-                            // value=" "
-                            onChange={formik.handleChange}
-                            // error={formik.touched.name && Boolean(formik.errors.name)}
-                            // helperText={formik.touched.name && formik.errors.name}
-                            fullWidth
-                            variant="standard"
-                        /> */}
-                        <TextField
-                            multiline
-                            rows={4}
-                            sx={{ marginTop: '25px' }}
-                            id="description"
-                            name="description"
-                            label="Description"
-                            // value=" "
-                            onChange={formik.handleChange}
-                            //  ={formik.touched.name && Boolean(formik.errors.name)}
-                            // helperText={formik.touched.name && formik.errors.name}
-                            fullWidth
-                            variant="standard"
-                        />
-                        {/* <TextField
-                            sx={{ marginTop: '25px' }}
-                            id="quantity"
-                            name="quantity"
-                            label="Quantity"
-                            // value=" "
-                            onChange={formik.handleChange}
-                            // error={formik.touched.name && Boolean(formik.errors.name)}
-                            // helperText={formik.touched.name && formik.errors.name}
-                            fullWidth
-                            variant="standard"
-                        /> */}
-
-                        {fields.length != 0 && (
+                        {fieldDataArray.length != 0 && (
                             <>
-                                {fields.map((d, index) => (
-                                    <>
-                                        <Grid container spacing={5}>
-                                            <Grid item xs={6}>
-                                                {/* <span style={{backsground:""}}>{index+1}</span> */}
+                                <Grid container spacing={4}>
+                                    {fieldDataArray.map((data, index) => (
+                                        <>
+                                            <Grid item xs={5}>
                                                 <TextField
-                                                    sx={{ marginTop: '25px', background: '' }}
                                                     id="field_name"
                                                     name="field_name"
-                                                    label="Field Name"
-                                                    // value=" "
+                                                    label="Metadata Name"
+                                                    value={data.fieldName}
                                                     onChange={(e) => {
                                                         handleFieldNameChange(e.target.value, index);
                                                     }}
-                                                    //  ={formik.touched.name && Boolean(formik.errors.name)}
-                                                    // helperText={formik.touched.name && formik.errors.name}
                                                     variant="standard"
+                                                    fullWidth
                                                 />
                                             </Grid>
 
-                                            <Grid item xs={6}>
+                                            <Grid item xs={5}>
                                                 <TextField
-                                                    sx={{ marginTop: '25px' }}
                                                     id="field_value"
                                                     name="field_value"
-                                                    label="Field Value"
-                                                    // value=" "
+                                                    label="Metadata Value"
+                                                    value={data.fieldValue}
                                                     onChange={(e) => {
                                                         handleFieldValueChange(e.target.value, index);
                                                     }}
-                                                    // error={formik.touched.name && Boolean(formik.errors.name)}
-                                                    // helperText={formik.touched.name && formik.errors.name}
                                                     variant="standard"
+                                                    fullWidth
                                                 />
                                             </Grid>
-                                        </Grid>
-                                    </>
-                                ))}
+                                            <Grid item xs={2} mt={2}>
+                                                <IconButton
+                                                    color="error"
+                                                    edge="end"
+                                                    size="small"
+                                                    onClick={() => {
+                                                        handleRemoveField(index);
+                                                    }}
+                                                >
+                                                    <Icon icon={closeFill} width={28} height={28} />
+                                                </IconButton>
+                                            </Grid>
+                                        </>
+                                    ))}
+                                </Grid>
                             </>
                         )}
 
-                        <div style={{ height: '3rem' }}>
-                            <Button
-                                variant="contained"
-                                sx={{ float: 'right', mt: 3 }}
-                                onClick={() => {
-                                    setFields([...fields, 1]);
-                                    setFieldDataArray([
-                                        ...fieldDataArray,
-                                        {
-                                            fieldName: '',
-                                            fieldValue: ''
-                                        }
-                                    ]);
-                                }}
-                            >
-                                Add Fields
-                            </Button>
-                        </div>
                         {uploadedImages.length !== 1 && (
                             <Grid
                                 sx={{ background: '#c5cbe9', borderRadius: '5px', paddingBottom: '2rem', paddingTop: '2rem' }}
@@ -403,7 +401,7 @@ export default function AddNft({ open, setOpen }) {
                             size="large"
                             disableElevation
                         >
-                            {'Add'}
+                            Add
                         </Button>
                     </AnimateButton>
                     <AnimateButton>
