1fb95480dd10c1d8e2467ba633f850f013a83e5e
DESKTOP-5HGFKUS\Mehtab <mehtabgilani512@gmail.com>
2022-11-29 15:59:27 +0500

    change

---
+++ src/views/pages/landingPage/component/FeaturedCreators.js
@@ -0,0 +1,135 @@
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Container, Grid, Typography } from '@mui/material';
+
+// project imports
+import FadeInWhenVisible from './Animation';
+import SubCard from 'ui-component/cards/SubCard';
+import Avatar from 'ui-component/extended/Avatar';
+import { gridSpacing } from 'store/constant';
+import shoes1 from 'assets/images/shoes1.png';
+import shoes2 from 'assets/images/shoes2.png';
+import shoes3 from 'assets/images/shoes3.png';
+import CardMedia from '@mui/material/CardMedia';
+import { Link as RouterLink } from 'react-router-dom';
+
+// =============================|| LANDING - FEATURE PAGE ||============================= //
+
+const FeaturedPage = () => {
+    const theme = useTheme();
+    const itemData = [
+        {
+            img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+            creations: '20 Creations',
+            title: 'Guli Silberstein'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+            creations: '20 Creations',
+            title: 'John Bumstead'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+            creations: '20 Creations',
+            title: 'Orban Isma'
+        },
+
+        {
+            img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+            creations: '20 Creations',
+            title: 'HorrorCustom'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+            creations: '20 Creations',
+            title: 'SuperSemi'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+            creations: '20 Creations',
+            title: 'SuperSemi'
+        }
+    ];
+    return (
+        <Grid container-fluid spacing={gridSpacing} sx={{ margin: '15px' }}>
+            <Grid item xs={12} lg={12} md={12}>
+                <Grid container spacing={2} sx={{ mb: 2 }}>
+                    <Grid item xs={12}>
+                        <Typography
+                            variant="h2"
+                            mt={4}
+                            component="div"
+                            sx={{ textAlign: { xs: 'center', md: 'left', sm: 'center' }, textTransform: 'capitalize' }}
+                        >
+                            Featured Creators
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+                <Grid container justifyContent="center" spacing={gridSpacing} sx={{ textAlign: 'center' }}>
+                    {itemData.map((item) => (
+                        <Grid
+                            item
+                            md={2}
+                            sm={6}
+                            component={RouterLink}
+                            to="/ProductDetails"
+                            sx={{
+                                color: theme.palette.mode === 'dark' ? 'white' : 'black',
+
+                                textDecoration: 'none'
+                            }}
+                        >
+                            <Grid container justifyContent="center">
+                                <Grid item md={6} xs={6}>
+                                    <CardMedia
+                                        component="img"
+                                        sx={{ height: { xs: '100%', md: '100%', sm: '100%' }, width: '90%' }}
+                                        image={shoes1}
+                                    />
+                                </Grid>
+                                <Grid item md={6} xs={6}>
+                                    <Grid item md={12} xs={12}>
+                                        <CardMedia component="img" height="auto" image={shoes2} />
+                                    </Grid>
+                                    <Grid item md={12} xs={12}>
+                                        <CardMedia sx={{ marginTop: '10px' }} component="img" height="auto" image={shoes3} />
+                                    </Grid>
+                                </Grid>
+
+                                <Grid
+                                    item
+                                    md={12}
+                                    xs={12}
+                                    sx={{
+                                        color: theme.palette.mode === 'dark' ? 'white' : '#404040',
+                                        background: theme.palette.mode === 'dark' ? '#181C1F' : 'white',
+                                        boxShadow: '0px 4px 4px rgb(0 0 0 / 49%)',
+                                        padding: '20px 10px'
+                                    }}
+                                >
+                                    <Grid item md={12} xs={12}   className='overflow'>
+                                        <Typography
+                                            variant="h3"
+                                            sx={{ textAlign: { xs: 'center', sm: 'center', md: 'left' }, marginBottom: '10px' }}
+                                        >
+                                            {item.title}
+                                        </Typography>
+                                    </Grid>
+
+                                    <Grid item md={12} xs={12} sx={{ textAlign: { xs: 'center', sm: 'center', md: 'left' } }}>
+                                        <Typography variant="body"> {item.creations}</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default FeaturedPage;
---
+++ src/views/pages/landingPage/component/New.js
@@ -0,0 +1,172 @@
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Container, Card, Grid, Typography, CardActionArea, CardContent, Divider } from '@mui/material';
+
+// project imports
+import FadeInWhenVisible from './Animation';
+import SubCard from 'ui-component/cards/SubCard';
+import Avatar from 'ui-component/extended/Avatar';
+import { gridSpacing } from 'store/constant';
+import WATCHES from 'assets/images/WATCHES.png';
+import CardMedia from '@mui/material/CardMedia';
+
+// assets
+import PaletteTwoToneIcon from '@mui/icons-material/PaletteTwoTone';
+import ReorderTwoToneIcon from '@mui/icons-material/ReorderTwoTone';
+import SpeedTwoToneIcon from '@mui/icons-material/SpeedTwoTone';
+import { Link as RouterLink } from 'react-router-dom';
+
+// =============================|| LANDING - FEATURE PAGE ||============================= //
+
+const NewPage = () => {
+    const theme = useTheme();
+    const itemData = [
+        {
+            img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+            heading: 'Zennie',
+            title: 'Luxury Cars',
+            creator: 'Creator'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+            heading: 'Zennie',
+            title: 'Luxury Shoes',
+            creator: 'Creator'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+            heading: 'Zennie',
+            title: 'Luxury Watches',
+            creator: 'Creator'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+            heading: 'Zennie',
+            title: 'Real Estate',
+            creator: 'Creator'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+            heading: 'Zennie',
+            title: 'Luxury Goods',
+            creator: 'Creator'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+            heading: 'Zennie',
+            title: 'Luxury Watches',
+            creator: 'Creator'
+        }
+    ];
+    return (
+        <Grid container-fluid spacing={gridSpacing} sx={{ background: '', margin: '15px' }}>
+            <Grid item xs={12} lg={12} md={12}>
+                <Grid container spacing={2} sx={{ mb: 2 }}>
+                    <Grid item xs={12}>
+                        <Typography
+                            variant="h2"
+                            mt={4}
+                            component="div"
+                            sx={{ textAlign: { xs: 'center', md: 'left', sm: 'center' }, textTransform: 'capitalize' }}
+                        >
+                            New & Trending
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+                <Grid container justifyContent="center" spacing={gridSpacing} sx={{ mt: 2, textAlign: 'center', paddingRight: '1%' }}>
+                    {itemData.map((item) => (
+                        <Grid
+                            item
+                            md={2}
+                            sm={6}
+                            component={RouterLink}
+                            to="/ProductDetails"
+                            sx={{ color: theme.palette.mode === 'dark' ? 'white' : 'black', textDecoration: 'none' }}
+                        >
+                            <Card
+                                sx={{
+                                    color: theme.palette.mode === 'dark' ? 'white' : '#404040',
+                                    background: theme.palette.mode === 'dark' ? '#181C1F' : 'white',
+                                    maxWidth: 365,
+                                    width: '105%',
+                                    boxShadow: '1px 2px 6px #d3d3d3',
+                                    borderRadius: '7px'
+                                }}
+                            >
+                                <CardActionArea>
+                                    <CardMedia component="img" height="200" image={item.img} />
+                                    <CardContent style={{ padding: '6%' }}>
+                                        <Grid container>
+                                            <Grid item xs={8} sx={{ textAlign: 'left' }}>
+                                                <span style={{ fontWeight: '550', fontSize: '130%' }}>{item.heading}</span>
+                                                <div className='overflow' style={{ marginTop: '5%' }}>{item.title}</div>
+                                            </Grid>
+                                            <Grid item xs={4}  sx={{ background: '' }}>
+                                                <span style={{ fontWeight: '50 !important ', fontSize: '110%', float: 'right' }}>
+                                                    {item.creator}
+                                                </span>
+                                            </Grid>
+                                        </Grid>
+
+                                        <Divider sx={{ mt: 2, mb: 2 }} />
+                                        <Grid container sx={{ background: '' }}>
+                                            <Grid item md={6} xs={12} className='overflow' sx={{ pt: 1, }}>
+                                                <span
+                                                    style={{
+                                                        background: theme.palette.mode === 'dark' ? 'black' : '#d9d9d9',
+                                                        padding: '3% 4%',
+                                                        borderRadius: '10%',
+                                                        color: 'white',
+                                                        fontSize: '80%'
+                                                    }}
+                                                >
+                                                    02h
+                                                </span>{' '}
+                                                :{' '}
+                                                <span
+                                                    style={{
+                                                        background: theme.palette.mode === 'dark' ? 'black' : '#d9d9d9',
+                                                        padding: '3% 4%',
+                                                        borderRadius: '10%',
+                                                        color: 'white',
+                                                        fontSize: '80%'
+                                                    }}
+                                                >
+                                                    25m
+                                                </span>{' '}
+                                                :{' '}
+                                                <span
+                                                    style={{
+                                                        background: theme.palette.mode === 'dark' ? 'black' : '#d9d9d9',
+                                                        padding: '3% 4%  ',
+                                                        borderRadius: '10%',
+                                                        color: 'white',
+                                                        fontSize: '80%'
+                                                    }}
+                                                >
+                                                    04s
+                                                </span>
+                                            </Grid>
+                                            <Grid item md={6} xs={12} className='overflow' sx={{ pl: 1, marginTop:{xs:'10px', md:'0'} , 
+                                              }}>
+                                                Current Bid
+                                                <div style={{ marginTop: '5%', fontSize: '110%' }}>
+                                                    <b>$2913.32</b>
+                                                </div>
+                                            </Grid>
+                                        </Grid>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default NewPage;
