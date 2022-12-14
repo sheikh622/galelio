fd595e6fb866557b76568521dd8ac22119c6086d
AimanZaheer04 <99002734+AimanZaheer04@users.noreply.github.com>
2022-12-14 15:45:43 +0500

    style

--- src/views/pages/user/landingPage/component/newAndTrending.js
+++ src/views/pages/user/landingPage/component/newAndTrending.js
@@ -13,7 +13,36 @@ const NewAndTrendingNfts = ({ nfts }) => {
         autoplay: true,
 arrows:'true',
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                fade: true,
+                infinite: true,
+                speed: 500,
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     };
     return (
         <Grid container-fluid spacing={gridSpacing} sx={{ background: '', margin: '15px' }}>
