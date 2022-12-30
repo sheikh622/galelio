b33473681490851ae07672108824bb00466c22e0
AimanZaheer04 <99002734+AimanZaheer04@users.noreply.github.com>
2022-12-30 15:25:40 +0500

    bug

--- src/layout/UserLayout/index.js
+++ src/layout/UserLayout/index.js
@@ -39,7 +39,10 @@ const UserLayout = () => {
 
             <Divider sx={{ borderBottomWidth: 1, border: '1px solid #ccc' }} />
 
-            <Grid item md={10} xs={12} sx={{ pl: 3 }}>
+            <Grid item md={10} xs={12} sx={{ pl: 3 ,  
+                background: theme.palette.mode === 'dark' ? 'black' : '#f3f3f3',
+                color: theme.palette.mode === 'dark' ? 'white' : '#404040'
+            }}>
                 <Footer />
             </Grid>
         </>
