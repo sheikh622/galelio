7499add4ac664440f63e9e1bd69fe915f280cc53
zain-argon <zain.butt@argonteq.com>
2023-02-09 18:19:42 +0500

    bugs

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,9 +3,9 @@ import axioss from 'axios';
 
 // export const API_URL = 'http://192.168.3.146:4000/api/v1/'; //dawood
 // export const API_URL = 'http://localhost:3000/api/v1/'; //local
-// export const API_URL = 'https://app.galileoprotocol.io/api/v1/';
+export const API_URL = 'https://app.galileoprotocol.io/api/v1/';
 // export const API_URL = 'http://192.168.1.193:4000/api/v1/'; //zain
-export const API_URL = 'http://localhost:4000/api/v1/'; //zain
+// export const API_URL = 'http://localhost:4000/api/v1/'; //zain
 // export const API_URL = 'http://galileoargon.tk/api/v1';
 
 
