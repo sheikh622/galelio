5f832e47c31260593f9f9a98045e042a6ce89293
umarfarooq32 <115708362+umarfarooq32@users.noreply.github.com>
2023-03-30 15:00:12 +0500

    slider added in header component

--- src/views/pages/user/landingPage/index.js
+++ src/views/pages/user/landingPage/index.js
@@ -16,7 +16,7 @@ const LandingPage = () => {
     return (
         <Grid item md={12} xs={12} lg={12} xl={12}>
             <Grid container-fluid>
-                <Grid  item md={12} xs={12} className="mainBackground" sx={{ border: '2px solid transparent' }}>
+                <Grid  item md={12} xs={12} >
                     <Header />
                 </Grid>
                 <Grid item md={12} xs={12}>
