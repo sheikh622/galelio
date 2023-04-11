f199ce739b9fed2ffb7109d62391174c5f316c22
zain-argon <zain.butt@argonteq.com>
2023-04-11 15:53:20 +0500

    tracking

--- src/views/pages/TrackNFT/TrackNFT.js
+++ src/views/pages/TrackNFT/TrackNFT.js
@@ -1,7 +1,7 @@
 import { Box, Grid, Stack } from '@mui/material';
 
 import '@fontsource/public-sans';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@mui/material';
 import { Typography } from '@mui/material';
 
@@ -10,9 +10,94 @@ import galileoLogo from '../../../assets/images/galileo-white.png';
 import { padding } from '@mui/system';
 import music from '../../../assets/vedio.mp4';
 import { useNavigate } from 'react-router-dom';
+import FactoryAbi from 'contractAbi/Factory.json';
+import FactoryAddress from 'contractAbi/Factory-address.json';
+import { ethers, utils } from 'ethers';
+const axios = require('axios');
+
+let graphQLURL = 'https://api.studio.thegraph.com/query/44351/factory-graph2/14';
 
 const TrackNFT = () => {
+    const [serialNo, setSerialNo] = useState("")
     const navigate = useNavigate();
+
+    const searchSerial = async () => {
+
+        console.log('serialNo',serialNo);
+        
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const factoryAddr = new ethers.Contract(FactoryAddress.address, FactoryAbi.abi, signer);
+        console.log('factoryAddr', factoryAddr);
+
+        // let res = await (
+        //     await factoryAddr.serials("GALGNS1")
+        // )?.wait();
+
+        let res = await factoryAddr.serials(serialNo);
+
+        console.log('res',res);
+
+        let address = res[0].toLowerCase()
+        address = `"${address}"`
+        let tokenId = parseInt(res._tokenId._hex)
+        tokenId=  tokenId.toString()
+
+        tokenId= `"${tokenId}"`
+
+        // let address = `"0x4600b6a0f068ae1283ed68792ff3f0a085b3f0ef"`;
+        // let tokenId = `"1"`;
+
+        console.log('address', address);
+        console.log('tokenId', tokenId);
+       
+        // console.log('resTokenId', resTokenId);
+
+        const main = async () => {
+            try {
+               const result= await axios.post(graphQLURL, {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Content-Type': 'application/json',     
+                        'X-Requested-With': 'XMLHttpRequest'
+                    },
+                    query: `{
+                          galileoProtocolDeployeds(first: 5) {
+                              id
+                              _mintingContract
+                              blockNumber
+                              blockTimestamp
+                            }
+                            roleAdminChangeds(first: 5) {
+                              id
+                              role
+                              previousAdminRole
+                              newAdminRole
+                            }
+                             handleMints(where: {collections: ${address}, _tokenId: ${tokenId}}, orderBy: blockTimestamp,){ 
+                              id 
+                              _minter
+                              _tokenId 
+                              blockTimestamp 
+                            }
+                            handleUpdateUris(where: {collections: ${address}, _tokenId: ${tokenId}}, orderBy: blockTimestamp,){ 
+                              id 
+                              _tokenId
+                              _tokenURI
+                              _updator
+                              blockTimestamp 
+                            }
+                          }
+                          `
+                });
+                console.log('Query result: \n', result.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        main();
+    };
 
     return (
         <Stack position={'relative'} sx={{ height: '100vh', overflow: 'hidden' }}>
@@ -47,7 +132,13 @@ const TrackNFT = () => {
                             Track your NFT history{' '}
                         </Typography>
                         <Box className="trackBox">
-                            <input className="trackInput" placeholder="GAL-BMW-1234" />
+                            <input className="trackInput" placeholder="GAL-BMW-1234" 
+
+                            onChange={(e)=>{
+                                setSerialNo(e.target.value)
+                            }}
+                            
+                            />
                             <Button
                                 sx={{ alignSelf: 'center !important' }}
                                 className="create"
@@ -55,7 +146,9 @@ const TrackNFT = () => {
                                 variant="contained"
                                 color="secondary"
                                 onClick={() => {
-                                    navigate('/tracking');
+                                    // navigate('/tracking');
+
+                                    searchSerial();
                                 }}
                             >
                                 Track
