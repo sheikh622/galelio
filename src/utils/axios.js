9b1af1398d6f7fe17d5702a77c7562087668eeb1
AimanZaheer04 <99002734+AimanZaheer04@users.noreply.github.com>
2023-05-19 18:42:00 +0500

    update resend option

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,9 +2,9 @@ import { FeedSharp } from '@mui/icons-material';
 import axioss from 'axios';
 // export const API_URL = 'http://192.168.3.146:4000/api/v1/'; //dawood
 // export const API_URL = 'http://localhost:4000/api/v1/'; //local
-// export const API_URL = 'https://app.galileoprotocol.io/api/v1/';
+export const API_URL = 'https://app.galileoprotocol.io/api/v1/';
 // export const API_URL = 'http://192.168.0.79:4000/api/v1/'; //zain
-export const API_URL = 'https://demo.galileoprotocol.io/api/v1/';
+// export const API_URL = 'https://demo.galileoprotocol.io/api/v1/';
 // export const API_URL = 'http://192.168.0.79:4000/api/v1/'; //zain
 // export const API_URL = 'http://192.168.1.190:4000/api/v1/'; //talha
 // export const API_URL = 'http://192.168.1.190/api/v1/'; //talha
